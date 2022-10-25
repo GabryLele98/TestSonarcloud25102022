2ac74437ec1704148a46e0876c1d4d10ff4dae91
gabrieleanselmi <gabriele@giulianoanselmi.it>
2022-10-25 20:15:31 +0200

    Code smell fixed - Ticket ID #1

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,10 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class HelloWorld {
     public static void main (String[] args) {
-        System.out.println("Hello World!\nI'm trying SonarCloud!");
+        //System.out.println("Hello World!\nI'm trying SonarCloud!");
+        Logger logger = Logger.getLogger(HelloWorld.class.getName());
+        logger.log(Level.INFO, "Hello World!\nI'm trying SonarCloud!");
     }
 }
